b270111447d1cac1c1d2b6e83de6c2278d3514c1
Ilija <ilijaristov67@gmail.com>
2026-02-17 22:17:53 +0100

    added active player need to do logic now

--- script.js
+++ script.js
@@ -7,18 +7,32 @@ let diceEl = document.querySelector('.dice');
 let btnNew = document.querySelector('.btn--new');
 let btnRoll = document.querySelector('.btn--roll');
 let btnHold = document.querySelector('.btn--hold');
+let currentScore0 = document.getElementById('current--0');
+let currentScore1 = document.getElementById('current--1');
 
 //Starting conditions
 score0El.textContent = '0';
 score1El.textContent = '0';
 diceEl.classList.add('hidden');
 
+const scores = [0, 0]
+let currentScore = 0;
+let activePlayer = 0;
 //game-logic
+
 btnRoll.addEventListener('click', function(){
   let dice = Math.trunc(Math.random()* 6) +1;
 
   diceEl.classList.remove('hidden');
 
   diceEl.src = `dice-${dice}.png`;
+
+
+  if(dice !== 1){
+    currentScore += dice;
+    currentScore0.textContent = currentScore;
+  }else{
+
+  }
 
 });
