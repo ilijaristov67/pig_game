b75eaa7ce213cb1a7189eaec7dfbd01500a32df1
Ilija <ilijaristov67@gmail.com>
2026-02-17 21:05:53 +0100

    added elements and class

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//Selecting elements for score
+let score0El = document.getElementById('score--0');
+let score1El = document.getElementById('score--1');
+let diceEl = document.querySelector('.dice');
+
+score0El.textContent = '0';
+score1El.textContent = '0';
+diceEl.classList.add('hidden');
