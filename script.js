e4040ebc54e233cf0a614d4743b85c08a06f27f3
Ilija <ilijaristov67@gmail.com>
2026-02-17 21:15:34 +0100

    added some more elements

--- script.js
+++ script.js
@@ -4,7 +4,11 @@
 let score0El = document.getElementById('score--0');
 let score1El = document.getElementById('score--1');
 let diceEl = document.querySelector('.dice');
+let btnNew = document.querySelector('.btn--new');
+let btnRoll = document.querySelector('.btn--roll');
+let btnHold = document.querySelector('.btn--hold');
 
+//Starting conditions
 score0El.textContent = '0';
 score1El.textContent = '0';
 diceEl.classList.add('hidden');
